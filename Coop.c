b9ddfe8fc26e922218ec722ee67f367092d4bf94
Bryce Dougherty <bryce.dougherty@gmail.com>
2015-12-13 21:47:39 -0800

    - Added Class Instances  - Created type identifiers  - Created debug logging defines  - Fix some memory bugs

--- Coop.c
+++ Coop.c
@@ -1,103 +1,207 @@
 #include "Coop.h"
 
 void initializeStuff();
+void initBaseClasses();
+
 
 
 int main()
 {
+	initBaseClasses();
 	initializeStuff();
 	
 	
 
-    
-    return 0;
+
+	return 0;
+}
+
+Class BDObject;
+
+void initBaseClasses() {
+
+	printf( COLOR_MAGENTA "\n\n\n========= INITIALIZING RUNTIME =========\n\n\n" COLOR_RESET "\n");
+	BDObject inheritsFrom(NULL);
+	BDObject.name = "BDObject";
+
+	classFunc(BDObject, new, {
+		return makeClassInstance(&BDObject);
+	});
+
+	classFunc(BDObject, newSubclass, {
+		return makeClass(&BDObject);
+	});
+
+	instanceFunction BDObject_printlol = makeInstanceFunction("printlol", ^(obj self, void* args) {
+		logPrint("lol");
+		//instance_do(self, "printlol", 0, NULL);
+	});
+	class_addInstanceFunction(&BDObject, &BDObject_printlol);
+
+	// instanceFunc(BDObject, printlol, {
+	// 	printf("lol");
+	// });
+		printf( COLOR_MAGENTA "\n\n\n========= RUNTIME INITIALIZED =========\n\n\n" COLOR_RESET "\n");
 }
 
 void initializeStuff() {
-	Class myClass inheritsFrom(NULL);
+	//Class myClass inheritsFrom(&BDObject);
 
-	func(myClass, checkStuffFunction, {
-		printf("checking to see if this thing works\n");
+	Class myClass = *(Class *)$(BDObject, newSubclass);
+	myClass.name = "myClass";
+
+	obj myClassInstance = $(BDObject, new);
+
+
+
+	classFunc(myClass, checkStuffFunction, {
+		logPrint("checking to see if this thing works\n");
 		return 420;
 	});
 
-	func(myClass, add4, {
+	classFunc(myClass, add4, {
 		int *array = (int *)args;
 		int added = array[0] + array [1];
-		printf("The numbers are %d and %d\n", array[0], array[1]);
+		logPrint("The numbers are %d and %d\n", array[0], array[1]);
 		return added;
 	});
+
+	generic_do(myClassInstance, "printlol", 0, NULL);
+	//$(myClassInstance, printlol);
 	int stuff[2] = { 4, 5 };
 	int returnedAddedValue = (int)$$(myClass, add4, 2, stuff);
 	int returnedValue = (int)$(myClass, checkStuffFunction);
 	printf("%d\n", returnedAddedValue);
+
+
 }
 
-void class_addFunction(Class *class, classFunction *function) {
-
-	// int oldSize = class.functionsSize;
-	// printf("old function size is %d\n", oldSize );
-	// printf("new size is %lu\n", sizeof(function) * (oldSize + 1));
-
-
+void class_addClassFunction(Class *class, classFunction *function) {
+	printf( COLOR_YELLOW " - adding %s to %s\n" COLOR_RESET, function->name, class->name);
 	int oldSize  = class->functionsSize;
-	realloc(class->functions, sizeof(classFunction) * (oldSize + 1));
-	// classFunction newArray[sizeof(classFunction) * (oldSize + 1)];// malloc(sizeof(function) * (oldSize + 1));
-	// //printf("%s\n", class.functions );
-	// int i;
-	// for (i=0; i<=oldSize; i++) {
-	// 	//*(&newArray + i) = *(&class.functions + i);
-	// 	newArray[i] = class->functions[i];
-	// }
-	//*(&newArray + oldSize) = &function;
-	//newArray[oldSize] = *function;
+	struct classFunction *newClassFunctions = realloc(class->functions, sizeof(classFunction) * (oldSize + 1));
+	if (newClassFunctions) {
+	class->functions = newClassFunctions;
 	class->functions[oldSize] = *function;
 	//class->functions = newArray;
 	class->functionsSize = oldSize + 1;
-
+} else {
+	errorLog("couldn't add %s to %s", function->name, class->name);
+}
 
 	
 }
+void class_addInstanceFunction(Class* class, instanceFunction *function) {
 
-void* class_do(Class *class, char* functionName, int number, void *passingArgs) {
+	printf( COLOR_YELLOW " - adding %s to %s\n" COLOR_RESET, function->name, class->name);
+	int oldSize = class->instanceFunctionsSize;
+	struct instanceFunction *newInstanceFunctions = realloc(class->instanceFunctions, sizeof(instanceFunction) * (oldSize + 1));
+	if (newInstanceFunctions) {
+		class->instanceFunctions = newInstanceFunctions;
+		class->instanceFunctions[oldSize] = *function;
+		class->instanceFunctionsSize = oldSize + 1;
+
+	}  
+	else {
+
+	}
+	
+}
 
 
-	printf("doing function %s\n", functionName);
+imp class_do(Class *class, char* functionName, int number, void *passingArgs) {
+
+
+	printf( COLOR_BLUE "doing function %s on Class %s" COLOR_RESET "\n", functionName, class->name);
 	classFunction* functions = class->functions;
 	int numberOfFunctions = class->functionsSize;
-	printf("there are %d functions\n", numberOfFunctions);
+	printf(" - there are %d functions\n", numberOfFunctions);
 	for (int i = 0; i < numberOfFunctions; ++i)
 	{
-		printf("checking function %d: %s\n", i+1, functions[i].name);
+		printf(" -- checking function %d: %s\n", i+1, functions[i].name);
 		if (functions[i].name == functionName)
 		{
-			printf("it's function %d\n", i+1);
-			void *(^asBlock)(void*) = (blockType) (functions[i]).implementation;
-			return asBlock(passingArgs);
+			printf(" --- it's function %d\n", i+1);
+			void *(^asBlock)(void*, void*) = (blockType) (functions[i]).implementation;
+			return asBlock(class, passingArgs);
 		}
 	}
 	return NULL;
 }
 
-classFunction makeFunction(char* name, void* implementation) {
+imp instance_do(obj instance, char* functionName, int number, void *passingArgs) {
 
+	Class *class = &instance->frame;
+	printf( COLOR_BLUE "doing function %s on instance of class %s" COLOR_RESET "\n", functionName, class->name);
+	
+	instanceFunction* functions = class->instanceFunctions;
+	int numberOfFunctions = class->instanceFunctionsSize;
+	printf(" - there are %d functions\n", numberOfFunctions);
+	for (int i = 0; i < numberOfFunctions; ++i)
+	{
+		printf(" -- checking function %d: %s\n", i+1, functions[i].name);
+		if (functions[i].name == functionName)
+		{
+			printf(" --- it's function %d\n", i+1);
+			void *(^asBlock)(void*, void*) = (blockType) (functions[i]).implementation;
+			return asBlock(instance, passingArgs);
+		}
+	}
+	return NULL;
+}
+
+imp generic_do(void* myObject, char* functionName, int number, void *passingArgs) {
+	logPrint("object type is %d", ((int*)myObject)[0]);
+	if (((int*)myObject)[0] == 1) {
+		return class_do(myObject, functionName, number, passingArgs);
+	} else if (((int*)myObject)[0] == 2){
+		return instance_do(myObject, functionName, number, passingArgs);
+	} else {
+		errorLog("error doing function %s", functionName);
+		return NULL;
+	}
+}
+
+
+
+classFunction makeClassFunction(char* name, imp implementation) {
+	printf( COLOR_GREEN " - making class function %s\n" COLOR_RESET, name);
 	classFunction newFunction;
 	newFunction.name = name;
 	newFunction.implementation = implementation;
 	return newFunction;
 }
 
+instanceFunction makeInstanceFunction(char* name, imp implementation) {
+	printf( COLOR_GREEN " - making instance function %s\n" COLOR_RESET, name);
+	instanceFunction newFunction;
+	newFunction.name = name;
+	newFunction.implementation = implementation;
+	return newFunction;
+}
 
 Class makeClass(Class *parent) {
+	printf( COLOR_RED "making subclass of %s" COLOR_RESET "\n", (parent ? parent->name : "nothing"));
 	return (Class) {
 		.inherits = parent,
 		.functions = malloc(sizeof(classFunction)),
+		.instanceFunctions = malloc(sizeof(instanceFunction)),
 		.functionsSize = 0,
+		.instanceFunctionsSize = 0,
 		.properties = malloc(sizeof(classProperty)),
-		.propertiesSize = 0
+		.propertiesSize = 0,
+		.structType = 1
+
 	};
 }
 
+obj makeClassInstance(Class *parent) {
+
+	obj intermediateObject = malloc(sizeof(*parent));
+	intermediateObject->frame = *parent;
+	intermediateObject->structType = 2;
+	return intermediateObject;
+}
 
 
 
--- Coop.h
+++ Coop.h
@@ -10,46 +10,93 @@
 #define PASTE2( a, b) a##b
 #define PASTE( a, b) PASTE2( a, b)
 
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_YELLOW  "\x1b[33m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN    "\x1b[36m"
+#define COLOR_RESET   "\x1b[0m"
+
 #define makeFunc(aname, aimp) (classFunction) {.name = #aname, .implementation = aimp}
-#define $(classMember, functionName) class_do(&classMember, #functionName, 0, NULL)
+#define $(classMember, functionName) class_do((void *)&classMember, #functionName, 0, NULL)
 #define $$(classMember, functionName, argsNumber, args) class_do(&classMember, #functionName, argsNumber, args)
 //#define ∞(parent)  = makeClass(parent)
 #define inheritsFrom(parent) = makeClass(parent)
 #define newClass(name, parent) Class name = makeClass(parent)
-#define func(classMember, name, imp) classFunction classMember ## _ ## name = makeFunction(#name, ^(void* args) imp); class_addFunction(&classMember, &classMember ## _ ## name);
+#define classFunc(classMember, name, imp) classFunction classMember ## _ ## name = makeClassFunction(#name, ^(Class* self, void* args) imp); class_addClassFunction(&classMember, &classMember ## _ ## name);
+#define instanceFunc(classMember, name, imp) instanceFunction classMember ## _ ## name = makeInstanceFunction(#name, ^(obj self, void* args) imp); class_addInstanceFunction(&classMember, &classMember ## _ ## name);
 #define msgSend(classMember, functionName) class_do(&classMember, #functionName)
+
+#define logPrint(args,...) printf( COLOR_CYAN "§ - "  args COLOR_RESET "\n", ## __VA_ARGS__)
+#define errorLog(args,...) printf( COLOR_MAGENTA "ERROR (%s,%s,%d): " args COLOR_RESET "\n", __FILE__, __func__, __LINE__, ##__VA_ARGS__)
+
+
+
 
 typedef struct classFunction{
     char *name;
     void *implementation;
 } classFunction;
 
+typedef struct instanceFunction{
+    char *name;
+    void *implementation;
+} instanceFunction;
+
 typedef struct classProperty{
     char *name;
     void *value;
 } classProperty;
 
+typedef void* imp;
+
+
+//Object type
+//1 == Class
+//2 == Instance
 
 typedef struct Class {
+	int structType;
 	char *name;
 	struct Class *inherits;
 	struct classFunction* functions;
 	int functionsSize;
+	struct instanceFunction* instanceFunctions;
+	int instanceFunctionsSize;
 	struct classProperty* properties;
 	int propertiesSize;
-
+	
 } Class;
 
-typedef void* (^blockType)(void*);
+typedef struct CoopObject {
+	int structType;
+	Class frame;
+
+} object;
+
+typedef object *obj;
+
+// typedef struct ClassInstance {
+// 	char *name;
+// 	struct Class *type;
+// }
+
+typedef void* (^blockType)(void*, void*);
 
 // void class_addFunction(Class *class, classFunction *function);
 // classFunction functionMake(char* name, void* implementation) ;
 
-void class_addFunction(Class *class, classFunction *function);
-classFunction makeFunction(char* name, void* implementation) ;
+void class_addClassFunction(Class *class, classFunction *function);
+void class_addInstanceFunction(Class *class, instanceFunction *function);
+classFunction makeClassFunction(char* name, void* implementation) ;
+instanceFunction makeInstanceFunction(char* name, void* implementation) ;
 Class makeClass(Class *parent);
+obj makeClassInstance(Class *parent);
 
-void* class_do(Class *class, char* functionName, int number, void *passingArgs);
-
+imp class_do(Class *class, char* functionName, int number, void *passingArgs);
+imp instance_do(obj instance, char* functionName, int number, void *passingArgs);
+imp generic_do(void* object, char* functionName, int number, void *passingArgs);
+int getType(void *object);
 
 #endif
